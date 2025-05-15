2ec64c392ae83b1c8e332e669be13f9132ff6c03
FilipCiesielsk1 <139149998+FilipCiesielsk1@users.noreply.github.com>
2025-05-15 11:58:45 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// assets/js/main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+});
